eaeb619fa707be3edeed5e34a387bc027acf91ff
lwthiker <lwt@lwthiker.com>
2022-03-10 11:57:53 +0200

    Add Alpine Linux build system
    Add Dockerfiles for building curl-impersonate on Alpine Linux.
    
    The Dockerfile template file was modified to support Alpine Linux,
    mainly by changing the dependency installation from 'apt' to 'apk'.
    The resulting alpine images are small (~11mb) and will be uploaded to
    Docker hub.

--- generate_dockerfiles.sh
+++ generate_dockerfiles.sh
@@ -3,11 +3,26 @@
 cat <<EOF | mustache - Dockerfile.template > chrome/Dockerfile
 ---
 chrome: true
+debian: true
+---
+EOF
+cat <<EOF | mustache - Dockerfile.template > chrome/Dockerfile.alpine
+---
+chrome: true
+alpine: true
 ---
 EOF
 
 cat <<EOF | mustache - Dockerfile.template > firefox/Dockerfile
 ---
 firefox: true
+debian: true
+---
+EOF
+
+cat <<EOF | mustache - Dockerfile.template > firefox/Dockerfile.alpine
+---
+firefox: true
+alpine: true
 ---
 EOF
