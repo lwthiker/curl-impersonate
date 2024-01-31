ef75d85ead31e6e8fa7f91f38f04fc44465d299b
Yifei Kong <kong@yifei.me>
2024-01-31 14:36:18 +0800

    Remove firefox from docker images

--- generate_dockerfiles.sh
+++ generate_dockerfiles.sh
@@ -12,17 +12,3 @@ chrome: true
 alpine: true
 ---
 EOF
-
-cat <<EOF | mustache - Dockerfile.template > firefox/Dockerfile
----
-firefox: true
-debian: true
----
-EOF
-
-cat <<EOF | mustache - Dockerfile.template > firefox/Dockerfile.alpine
----
-firefox: true
-alpine: true
----
-EOF
